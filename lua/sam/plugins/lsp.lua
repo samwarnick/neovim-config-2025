2f19e5cb415ff38c778989b933714e5e846362e8
Sam Warnick <sam@warnick.me>
2025-08-11 10:28:42 -0400

    LSP and formatting

---
+++ lua/sam/plugins/lsp.lua
@@ -0,0 +1,121 @@
+return {
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/nvim-cmp",
+		"mason-org/mason.nvim",
+		"mason-org/mason-lspconfig.nvim",
+	},
+	config = function()
+		-- Add cmp_nvim_lsp capabilities settings to lspconfig
+		-- This should be executed before you configure any language server
+		local lspconfig_defaults = require("lspconfig").util.default_config
+		lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+			"force",
+			lspconfig_defaults.capabilities,
+			require("cmp_nvim_lsp").default_capabilities()
+		)
+
+		-- This is where you enable features that only work
+		-- if there is a language server active in the file
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "LSP actions",
+			callback = function(event)
+				local opts = { buffer = event.buf }
+
+				vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+				vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+				vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+				vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+				vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+				vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+				vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+				vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+				vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+				vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+			end,
+		})
+
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+				lua_ls = function()
+					require("lspconfig").lua_ls.setup({
+						settings = {
+							Lua = {
+								telemetry = {
+									enable = false,
+								},
+							},
+						},
+						on_init = function(client)
+							local join = vim.fs.joinpath
+							local path = client.workspace_folders[1].name
+
+							-- Don't do anything if there is project local config
+							if
+								vim.uv.fs_stat(join(path, ".luarc.json"))
+								or vim.uv.fs_stat(join(path, ".luarc.jsonc"))
+							then
+								return
+							end
+
+							local nvim_settings = {
+								runtime = {
+									-- Tell the language server which version of Lua you're using
+									version = "LuaJIT",
+								},
+								diagnostics = {
+									-- Get the language server to recognize the `vim` global
+									globals = { "vim" },
+								},
+								workspace = {
+									checkThirdParty = false,
+									library = {
+										-- Make the server aware of Neovim runtime files
+										vim.env.VIMRUNTIME,
+										vim.fn.stdpath("config"),
+									},
+								},
+							}
+
+							client.config.settings.Lua =
+								vim.tbl_deep_extend("force", client.config.settings.Lua, nvim_settings)
+						end,
+					})
+				end,
+			},
+		})
+
+		---
+		-- Autocompletion config
+		---
+		local cmp = require("cmp")
+
+		cmp.setup({
+			sources = {
+				{ name = "nvim_lsp" },
+			},
+			mapping = cmp.mapping.preset.insert({
+				-- `Enter` key to confirm completion
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+
+				-- Ctrl+Space to trigger completion menu
+				["<C-Space>"] = cmp.mapping.complete(),
+
+				-- Scroll up and down in the completion documentation
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
+			}),
+			snippet = {
+				expand = function(args)
+					vim.snippet.expand(args.body)
+				end,
+			},
+		})
+	end,
+}
