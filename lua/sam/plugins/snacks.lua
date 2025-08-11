5a72936e76aff5b664c278a3b1d5fa1887d7cd8c
Sam Warnick <sam@warnick.me>
2025-08-11 09:34:42 -0400

    Initial commit

---
+++ lua/sam/plugins/snacks.lua
@@ -0,0 +1,18 @@
+-- 
+return {
+	{
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    explorer = {
+	    replace_netrw = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+        }
+      }
+    }
+  }
+}
+}
